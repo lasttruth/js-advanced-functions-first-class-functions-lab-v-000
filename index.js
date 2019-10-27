3db7f5bd01878ed3e9e6e4d11e8d9c820f3aad0d
Robin Doirin <robindoirin@gmail.com>
2019-10-27 00:37:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(drivers, driversFn) {
-  return driversFn(drivers);
+const selectDifferentDrivers = (driversArray, driversFunction) => {
+		return driversFunction(driversArray);
 }
